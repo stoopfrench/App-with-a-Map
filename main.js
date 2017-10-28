7146fab6bd0f80ebd1b0aa37faebbb43ece342d2
stoopfrench <32528005+stoopfrench@users.noreply.github.com>
2017-10-28 03:11:45 -0600

    all core components are working

--- main.js
+++ main.js
@@ -3,9 +3,8 @@ var mainVm = new Vue({
 	el: "#appMap",
 	data: {
 		markers: [],
+		notes: [],
 		
-
-
 	
 	},
 
@@ -13,35 +12,38 @@ var mainVm = new Vue({
 
 		placeMarker: function() {
 
-			
-
 			var positionX = event.pageX + 'px'
 			var positionY = event.pageY + 'px'
 
 			this.markers.push({positionX, positionY})
 
-			// this.divs.push('#marker')
-
-			// console.log(mainVm.markers)
-
-
-			// console.log(this.divs)
-
-			// console.log(positionX,positionY)
-			
-			console.log('x = ' + positionX, 'y = ' + positionY)
 			
 		},
 
-		hideMarker: function(marker) {
+		placeNote: function() {
+
+			var positionX = event.pageX + 'px'
+			var positionY = event.pageY + 'px'
+
+			this.notes.push({positionX, positionY})
+
+			// console.log(this.notes)
+
+		},
+
+		remove: function(marker) {
 
 			var index = this.markers.indexOf(marker)
 
 			this.markers.splice(index,1)
 
+			// var noteIndex = this.notes.indexOf(note)
 
-		}
+			this.notes.splice(index,1)
+		},
 	
+
+
 	},
 	
 })
